3c848364f5c018bd003319c1db336b761110e04b
lee212 <hroe.lee@gmail.com>
2013-10-09 01:47:56 -0400

    added counting function

--- ccstats/ccstats.py
+++ ccstats/ccstats.py
@@ -2,16 +2,17 @@ from db import ccsDB
 
 class ccstats:
     def __init__(self):
-        self.dbconn = ccsDB()
+        self.db = ccsDB()
+        self.dbconn = self.db.conn.metric.instance
 
     def stats_all(self):
         return
 
     def count_all(self):
-        return self.dbconn.instance.count()
+        return self.dbconn.count()
 
     def count_sierra_openstack_grizzly(self):
-        return self.dbconn.instance.find({"cloudPlatformIdRef":10}).count()
+        return self.dbconn.find({"cloudPlatformIdRef":10}).count()
         
 
 
