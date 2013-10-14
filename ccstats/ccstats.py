29c614fd436659c2da240d7e600e87ade4263042
lee212 <hroe.lee@gmail.com>
2013-10-14 15:38:17 -0400

    added store/read db function for measured usage data

--- ccstats/ccstats.py
+++ ccstats/ccstats.py
@@ -1,5 +1,6 @@
 from bson.code import Code
 from datetime import date, timedelta
+import json
 
 from db import ccsDB
 
@@ -7,6 +8,7 @@ class ccstats:
     def __init__(self):
         self.db = ccsDB()
         self.dbconn = self.db.conn.metric.instance
+        self.dbconn_stats = self.db.conn.db.ccstats
 
     # Experiments & test
     def stats_all(self):
@@ -63,19 +65,45 @@ class ccstats:
                             "cloudPlatformIdRef":10})
 
     # search date by weekly, monthly, quarterly
-    def weekly_report(self):
-        cond = self.weekly_condition()
-        self.weekly_count(cond)
-        self.weekly_walltime(cond)
-        self.weekly_usercount(cond)
+    def generate_weekly_report(self):
+        cond = self.get_condition()
+        count = self.weekly_count(cond)
+        walltime = self.weekly_walltime(cond)
+        usercount = self.weekly_usercount(cond)
+        res = {"count":count,\
+               "walltime": walltime,\
+               "usercount": usercount,
+               "condition": json.dumps(cond)}
+        return res
+
+    def get_condition(self):
+        cond1 = self.weekly_condition()
+        cond2 = self.host_condition("sierra_openstack_grizzly")
+        cond = dict(cond1.items() + cond2.items())
+        return cond
+
+    def store_stats(self, data):
+        self.set_stats(data)
+
+    def read_stats(self):
+        self.get_stats()
+
+    def set_stats(self, data):
+        id = self.dbconn_stats.insert(data)
+        return id
+
+    def host_condition(self, name):
+        cond = {"cloudPlatformIdRef":10}
+        return cond
 
     def weekly_condition(self):
+        #print "=" * 50
+        #print "Start - End: " + str(seven_days_ago) + " - " + str(today)
+        #print "=" * 50
+
         # Condition for 7 days
         seven_days_ago = self.get_days_ago(7)
         today = self.get_days_ago(0)
-        print "=" * 50
-        print "Start - End: " + str(seven_days_ago) + " - " + str(today)
-        print "=" * 50
 
         # Total usage
         cond = { "t_start": \
@@ -87,13 +115,14 @@ class ccstats:
     def weekly_count(self, cond):
         # total_vm_count_lanched_during_a_last_week
         total_vm_count = self._count(cond)
-        print total_vm_count
 
-        res = self._group("state", cond)
-        print res
+        total_vm_count_by_state = self._group("state", cond)
         # Daily usage
-        res2 = self._group("date", cond)
-        print res2
+        total_vm_count_by_date = self._group("date", cond)
+        res = {"total": total_vm_count,\
+               "by_state": total_vm_count_by_state,\
+               "by_date": total_vm_count_by_date}
+        return res
 
     def weekly_walltime(self, cond):
 
@@ -116,14 +145,16 @@ class ccstats:
                         """)
 
         total_walltime = self._group("", cond, reducer)
-        print total_walltime
  
         daily_walltime = self._group("date", cond, reducer)
-        print daily_walltime
+
+        res = {"total": total_walltime,\
+               "by_date": daily_walltime}
+        return res
 
     def weekly_usercount(self, cond):
         key = "$ownerId"
-        res = self.dbconn.aggregate([{"$match": cond}, \
+        total = self.dbconn.aggregate([{"$match": cond}, \
                                {"$group": {"_id": key }}, \
                                            #"tot": {"$sum": 1}}}, \
                                {"$group": {"_id": 1, \
@@ -138,8 +169,11 @@ class ccstats:
                        }
                        prev.count = prev.list.length;
                       }""")
-        res2 = self._group("date", cond, reducer)
-        print res2
+        total_by_date = self._group("date", cond, reducer)
+
+        res = {"total": total,\
+               "by_date": total_by_date}
+        return res
 
     def get_days_ago(self, days_to_subtract):
         return str(date.today() - timedelta(days=days_to_subtract))
